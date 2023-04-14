3c9b357f9234b9844ac960598e759bef8e4cfa52
abdrah124 <abdmanharits@gmail.com>
2023-04-14 15:54:10 +0700

    editing some files add pagintion value features

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const limit = 50;
 import { P, createUI, displayData } from "./utils.js";
 let count = 0;
+const popkedexLimit = 900;
+let paging = 1;
+const pagination = popkedexLimit / limit;
+const paginValue = document.querySelector(".paginvalue");
 const next = document.querySelector(".next");
-next.addEventListener("click", function () {
-  if (count + limit < 900 - limit && count > -1) {
+next.addEventListener("click", async function () {
+  if (count + limit < popkedexLimit - limit && count > -1) {
     count += limit;
-  } else if (count + limit == 900 - limit) {
-    count = 900 - limit;
-  } else if (count + limit == 900) {
-    count = 900 - limit;
+    paging += 1;
+  } else if (count + limit == popkedexLimit - limit) {
+    count = popkedexLimit - limit;
+    paging !== pagination ? (paging += 1) : paging;
+    displayPagin();
+  } else if (count + limit == popkedexLimit) {
+    count = popkedexLimit - limit;
+    paging !== pagination ? (paging += 1) : paging;
+    displayPagin();
   }
-  displayData(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${count}`);
+  await displayData(
+    `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${count}`
+  );
+  displayPagin();
 });
 
 const prev = document.querySelector(".previous");
-prev.addEventListener("click", function () {
+prev.addEventListener("click", async function () {
   if (count - limit > -1) {
     count -= limit;
+    paging -= 1;
+    displayPagin();
   }
-  displayData(
+  await displayData(
     `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${
       count - limit > -1 ? count : 0
     }`
@@ -27,4 +41,9 @@ prev.addEventListener("click", function () {
 
 document.body.onload = function () {
   displayData(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${count}`);
+  displayPagin();
 };
+
+function displayPagin() {
+  paginValue.innerHTML = `Pages ${paging} of ${pagination} `;
+}
