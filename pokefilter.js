4b0d75e124c5c34fa9245f58b020135f0b8651ae
abdrah124 <abdmanharits@gmail.com>
2023-04-14 16:58:18 +0700

    adding more powerful search feature

--- pokefilter.js
+++ pokefilter.js
@@ -1,56 +1,60 @@
 const buttonSrc = document.querySelector(".button");
 const seVal = document.querySelector(".search");
 const table = document.querySelector(".pokelist tbody");
+
+const url = `https://pokeapi.co/api/v2/pokemon?limit=900&offset=0`;
 buttonSrc.addEventListener("click", async function () {
-  try {
-    if (
-      seVal.value === undefined ||
-      seVal.value.length == 0 ||
-      seVal.value == ""
-    ) {
-      throw new Error("Can't found the Pokemon");
-    }
-    const UI = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${seVal.value.toLowerCase()}`
-    )
-      .then((a) => {
-        if (!a.ok) {
-          throw new Error("Can't found the Pokemon");
-        }
-        return a.json();
-      })
-      .then((e) => {
-        if (e === undefined) {
-          throw new Error("Can't found the Pokemon");
-        }
-        return ` <tr> 
-    <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${
-      e.id ? e.id : "25"
-    }.png" alt="${e.name}">
-   </td> 
-     <td>0${
-       e.id
-     }</td> <td><a href="#modalDetail"><span class="pokelist po" data-name="${
-          e.name
-        }" data-id="${e.id}">${toUp(e.name)}</span></a></td>
-      <td>
-      <img src="img/${e.types[0].type.name}.png" alt="${
-          e.types[0].type.name
-        }" width="95px"> 
-      <br>
-      ${
-        e.types[1]?.type?.name !== undefined
-          ? `<img src="img/${e.types[1].type.name}.png" alt="${e.types[1].type.name}" width="95px">`
-          : ``
-      }
-    </td>
-  </tr>`;
-      });
-    table.innerHTML = UI;
-  } catch (err) {
-    seVal.value = err.message;
-    setTimeout(() => {
-      seVal.value = "";
-    }, 2000);
-  }
+  const data = await getSearchData(url);
+  const filterData = await filterSearchData(data, seVal.value);
+  const UI = await createUI(filterData).join("");
+  table.innerHTML = UI;
 });
+
+function getSearchData(url) {
+  return fetch(url)
+    .then((a) => a.json())
+    .then((a) => {
+      const promises = a.results.map(async (a) => {
+        const r = await fetch(a.url);
+        return r.json();
+      });
+      return Promise.all(promises);
+    });
+}
+function filterSearchData(data, input) {
+  return data.filter((a) => {
+    return a.name.startsWith(input);
+  });
+}
+async function testing() {
+  const data = await getSearchData(url);
+  const filterData = await filterSearchData(data, "yve");
+  const UI = await createUI(filterData).join("");
+  table.innerHTML = UI;
+}
+function createUI(filterData) {
+  return filterData.map(
+    (e) => ` <tr> 
+  <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${
+    e.id ? e.id : "25"
+  }.png" alt="${e.name}">
+ </td> 
+   <td>0${
+     e.id
+   }</td> <td><a href="#modalDetail"><span class="pokelist po" data-name="${
+      e.name
+    }" data-id="${e.id}">${toUp(e.name)}</span></a></td>
+    <td>
+    <img src="img/${e.types[0].type.name}.png" alt="${
+      e.types[0].type.name
+    }" width="95px"> 
+    <br>
+    ${
+      e.types[1]?.type?.name !== undefined
+        ? `<img src="img/${e.types[1].type.name}.png" alt="${e.types[1].type.name}" width="95px">`
+        : ``
+    }
+  </td>
+</tr>`
+  );
+}
